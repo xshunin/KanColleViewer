2ee6b8f13c9c689a573219eade268829d56a08dc
Danny Vargas <danvargas404@gmail.com>
2014-03-20 21:51:41 -0400

    Release v2.4.0.364

--- Grabacr07.KanColleViewer/Properties/AssemblyInfo.cs
+++ Grabacr07.KanColleViewer/Properties/AssemblyInfo.cs
@@ -8,10 +8,10 @@ using System.Windows;
 // アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
 [assembly: AssemblyTitle("KanColleViewer!")]
-[assembly: AssemblyDescription("KanColleViewer! r352")]
+[assembly: AssemblyDescription("KanColleViewer!")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("grabacr.net")]
-[assembly: AssemblyProduct("KanColleViewer! r352")]
+[assembly: AssemblyProduct("KanColleViewer!")]
 [assembly: AssemblyCopyright("Copyright © 2013 Grabacr07")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -51,4 +51,4 @@ using System.Windows;
 // すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.4.0.352")]
+[assembly: AssemblyVersion("2.4.0.364")]
